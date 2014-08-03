c61ed9a72ccb8a97ba0a7370602a8352748979ca
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 16:09:26 +0200

    Added test for displaying forecasts index

--- spec/views/forecasts/index.html.erb_spec.rb
+++ spec/views/forecasts/index.html.erb_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+
+describe "Displaying index" do
+  before{
+    create :forecast
+  }
+  it "displays the created forecasts" do
+    visit 'forecasts'
+    expect(page).to have_content "Location"
+    expect(page).to have_content "Latitude"
+    expect(page).to have_content "Longitude"
+    expect(page).to have_content "DE24 3JG"
+    expect(page).to have_content "52.875771"
+    expect(page).to have_content "-1.505059"
+  end
+end
