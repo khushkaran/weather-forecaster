5a457846521cc7dbb5eb76cad5704891e3074487
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 14:51:09 +0200

    Added geocode stub

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,9 +1,14 @@
 require 'rails_helper'
 
+Geocoder.configure(:lookup => :test)
+Geocoder::Lookup::Test.add_stub("DE24 3JG", [ { 'latitude' => 52.875771, 'longitude' => -1.505059 } ])
+
 describe Forecast do
+  before{
+    @forecast = create :forecast
+  }
   it "knows its lattitude and longitude" do
-    forecast = create :forecast
-    expect(forecast.latitude.round(6)).to eq 52.875771
-    expect(forecast.longitude.round(6)).to eq -1.505059
+    expect(@forecast.latitude.round(6)).to eq 52.875771
+    expect(@forecast.longitude.round(6)).to eq -1.505059
   end
 end
