14708740540d1104b11053a904b4b2b81722d110
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 17:10:27 +0200

    Added spec for forecast view

--- spec/views/forecasts/show.html.erb_spec.rb
+++ spec/views/forecasts/show.html.erb_spec.rb
@@ -1,1 +1,11 @@
 require 'rails_helper'
+
+describe "Viewing a forecast" do
+  before{
+    @forcast = create: :forecast   
+  }
+  it "displays the title" do
+    visit forecast_path(FIRSTFORECAST)
+    expect(page).to have_content "Forecast for DE24 3JG"
+  end
+end
