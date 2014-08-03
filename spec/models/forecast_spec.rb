4316393d36f3dcf7165a2939eb7283a1b4af0194
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 14:11:30 +0200

    Added forecast test for longitude and latitude

--- spec/models/forecast_spec.rb
+++ spec/models/forecast_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
-RSpec.describe Forecast, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Forecast do
+  it "knows its lattitude and longitude" do
+    forecast = create :forecast
+    expect(forecast.latitude.round(6)).to eq 52.875771
+    expect(forecast.longitude.round(6)).to eq -1.505059
+  end
 end
