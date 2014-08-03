05b8f7bcb69cf43cec4ed43c97914c7d75652503
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 15:50:46 +0200

    included cabybara dsl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,7 @@ RSpec.configure do |config|
   # instead of true.
   config.use_transactional_fixtures = true
   config.include FactoryGirl::Syntax::Methods
+  config.include Capybara::DSL
 
   config.before(:each) do
     stub_request(:get, "https://api.forecast.io/forecast/dbc88215754b7c7e42505af85dfef469/52.875771,-1.505059?units=uk").
