e73e9d62195f01e9df97fbf787b3d459d4cf87c2
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 15:51:18 +0200

    Added test for input box

--- spec/views/forecasts/new.html.erb_spec.rb
+++ spec/views/forecasts/new.html.erb_spec.rb
@@ -1,1 +1,8 @@
 require 'rails_helper'
+
+describe "New Forecast" do
+  it "has a input box for location" do
+    visit 'forecasts/new'
+    expect(page).to have_css "input.forecast_location"
+  end
+end
