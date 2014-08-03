b87b4e0f27d90c739592c38fe9be424dbc0d89cd
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 15:56:22 +0200

    Added redirection test on creation of forecast

--- spec/views/forecasts/new.html.erb_spec.rb
+++ spec/views/forecasts/new.html.erb_spec.rb
@@ -1,8 +1,17 @@
 require 'rails_helper'
 
 describe "New Forecast" do
-  it "has a input box for location" do
+  before{
     visit 'forecasts/new'
+  }
+
+  it "has a input box for location" do
     expect(page).to have_css "input.forecast_location"
+  end
+
+  it "redirects to the show page on creation" do
+    fill_in "forecast_location", with: "DE24 3JG"
+    click_button 'Get Forecast'
+    expect(page).to have_css "h1", text: "DE24 3JG"
   end
 end
