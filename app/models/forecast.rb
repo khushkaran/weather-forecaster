c28597dd9aff1021ce18d536b742180ce5f42277
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 15:30:02 +0200

    Added while loop to wait for latitude and longitude to be populated

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -4,6 +4,9 @@ class Forecast < ActiveRecord::Base
   serialize(:api_data, Hash)
 
   def api_data
+    while self.latitude == nil or self.longitude == nil
+      wait(1)
+    end
     ForecastIO.api_key = Rails.application.secrets.forecast_io_api_key
     ForecastIO.forecast(self.latitude, self.longitude, params:{units: 'uk'})
   end
