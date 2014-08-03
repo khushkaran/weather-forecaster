b0b98834e5ca9febe4ba11a0000ceedf4612e856
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 17:15:14 +0200

    Added spec for current conditions

--- spec/views/forecasts/show.html.erb_spec.rb
+++ spec/views/forecasts/show.html.erb_spec.rb
@@ -3,9 +3,16 @@ require 'rails_helper'
 describe "Viewing a forecast" do
   before{
     @forecast = create :forecast   
-  }
-  it "displays the title" do
     visit forecast_path(@forecast)
+  }
+
+  it "displays the title" do
     expect(page).to have_content "Forecast for DE24 3JG"
+  end
+
+  it "displays the current weather" do
+    expect(page).to have_content "Current Conditions"
+    expect(page).to have_content "Partly Cloudy"
+    expect(page).to have_content "Feels Like 16"
   end
 end
