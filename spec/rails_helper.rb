1aea6fe9f5d822a57f623e0363152bfaa55fd772
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 14:40:18 +0200

    Disabled net connection

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
+require 'webmock/rspec'
+WebMock.disable_net_connect!(allow_localhost: true)
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
