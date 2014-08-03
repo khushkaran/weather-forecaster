dfcbaeb14eb0c6e1d471abf77c0fadd607d936d2
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-08-03 16:48:37 +0200

    Added test for add link on index page

--- spec/views/forecasts/index.html.erb_spec.rb
+++ spec/views/forecasts/index.html.erb_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 describe "Displaying index" do
   before{
     create :forecast
+    visit 'forecasts'
   }
   it "displays the created forecasts" do
-    visit 'forecasts'
     expect(page).to have_content "Location"
     expect(page).to have_content "Latitude"
     expect(page).to have_content "Longitude"
@@ -15,10 +15,13 @@ describe "Displaying index" do
   end
 
   it "a forecast can be deleted" do
-    visit 'forecasts'
     click_link 'Delete'
     expect(page).not_to have_content "DE24 3JG"
     expect(page).not_to have_content "52.875771"
     expect(page).not_to have_content "-1.505059"
+  end
+
+  it "has a link for creating a new forecast" do
+    expect(page).to have_content "Create Forecast"
   end
 end
